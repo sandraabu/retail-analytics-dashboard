cb7a93b3a6ce96c88bd255316397a236be267dff
Sandra Abubakir <83399849+sandraabu@users.noreply.github.com>
2022-07-17 09:56:58 +0100

    Add files via upload

--- retail_dash.py
+++ retail_dash.py
@@ -24,14 +24,14 @@ df['month_year'] = df['Year'].astype(str) + '-' + df['Month']
 
 df['Profit Margin'] = round((df['Profit']/df['Sales']) * 100,2)
 
-################################ total sales,profit, dicount Month level  ###################
+######################## total sales,profit, dicount, margin Month level  ###################
 monthly_sales_df = df.groupby(['month_year']).agg({'Sales':'sum'}).reset_index()
 monthly_profit_df = df.groupby(['month_year']).agg({'Profit':'sum'}).reset_index()
 monthly_discount_df = df.groupby(['month_year']).agg({'Discount':'sum'}).reset_index()
 monthly_margin_df = df.groupby(['month_year']).agg({'Profit Margin':'sum'}).reset_index()
 
  
-############################## rounding sales,profit,discount to 1 decimal ##################
+######################### rounding sales,profit,discount,margin to 1 decimal ##################
 monthly_sales_df['Sales'] = monthly_sales_df['Sales'].round(1)
 monthly_profit_df['Sales'] = monthly_profit_df['Profit'].round(1)
 monthly_discount_df['Sales'] = monthly_discount_df['Discount'].round(1)
@@ -51,19 +51,29 @@ dept_df['Sales'] = dept_df['Sales'].round(1)
 
 ######################### subcategory level sales ###############
 sub_dept_df=df.groupby(['month_year','Sub Category']).agg({'Sales':'sum'}).reset_index()
-#sub_dept_df['Sub Category'] = sub_dept_df['Sub Category'].apply(lambda x: 'Dept'+" "+str(x))
 sub_dept_df['Sales'] = sub_dept_df['Sales'].round(1)
 
+######################### region level sales ###############
+region_df=df.groupby(['month_year','Region']).agg({'Sales':'sum'}).reset_index()
+region_df['Sales'] = region_df['Sales'].round(1)
 
+######################### region level sales ###############
+region_df=df.groupby(['month_year','Region']).agg({'Sales':'sum'}).reset_index()
+region_df['Sales'] = region_df['Sales'].round(1)
+
+######################### city level sales ###############
+city_df=df.groupby(['month_year','City']).agg({'Sales':'sum'}).reset_index()
+city_df['Sales'] = city_df['Sales'].round(1)
+
+######################### orders level sales ###############
+order_df=df.groupby(['month_year','Order ID']).agg({'Sales':'sum'}).reset_index()
+order_df['Sales'] = order_df['Sales'].round(1)
 
 app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
-
-PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
 
 navbar = dbc.Navbar( id = 'navbar', children = [
     dbc.Row([
-        dbc.Col(html.Img(src = PLOTLY_LOGO, height = "70px")),
         dbc.Col(
             dbc.NavbarBrand("Retail Sales Dashboard", style = {'color':'white', 'fontSize':'50px','fontFamily':'woff2'}
                             )
@@ -111,21 +121,12 @@ card_content_dropdwn = [
             ],
         value = '2015-Jan',
         
-        )
-                    
-                    
+        )   
                     ]),
                 
-                
-                
-                
                 ])
-            
             ]
         )
-    
-    
-    
     ]
 
 
@@ -151,16 +152,16 @@ body_app = dbc.Container([
         dbc.Col([dbc.Card(id = 'card_num5',style={'height':'150px'})]),
         dbc.Col([dbc.Card(id = 'card_num6',style={'height':'150px'})]),
         dbc.Col([dbc.Card(id = 'card_num7',style={'height':'150px'})]),
-
+        dbc.Col([dbc.Card(id = 'card_num8',style={'height':'150px'})]),
         ]),
     
     html.Br(),
     html.Br(),
     
     dbc.Row([
-        dbc.Col([dbc.Card(id = 'card_num8',style={'height':'350px'})]),
         dbc.Col([dbc.Card(id = 'card_num9',style={'height':'350px'})]),
         dbc.Col([dbc.Card(id = 'card_num10',style={'height':'350px'})]),
+        dbc.Col([dbc.Card(id = 'card_num11',style={'height':'350px'})]),
         
         ]),
     
@@ -168,9 +169,9 @@ body_app = dbc.Container([
     html.Br(),
     
     dbc.Row([
-        dbc.Col([dbc.Card(id = 'card_num11',style={'height':'350px'})],width = 8),
-        dbc.Col([dbc.Card(id = 'card_num12',style={'height':'350px'})]),
+        dbc.Col([dbc.Card(id = 'card_num12',style={'height':'350px'})],width = 6),
         dbc.Col([dbc.Card(id = 'card_num13',style={'height':'350px'})]),
+        dbc.Col([dbc.Card(id = 'card_num14',style={'height':'350px'})]),
         
 
         ]),
@@ -198,11 +199,15 @@ app.layout = html.Div(id = 'parent', children = [navbar,body_app])
                Output('card_num8', 'children'),
                Output('card_num9', 'children'),
                Output('card_num10', 'children'),
-               Output('card_num11', 'children')
+               Output('card_num11', 'children'),
+               Output('card_num12', 'children'),
+               Output('card_num13', 'children'),
+               Output('card_num14', 'children')
                
                ],
               [Input('dropdown_base','value'), 
                 Input('dropdown_comp','value')])
+
 def update_cards(base, comparison):
     
     sales_base = monthly_sales_df.loc[monthly_sales_df['month_year']==base].reset_index()['Sales'][0]
@@ -226,12 +231,17 @@ def update_cards(base, comparison):
     diff_3 = np.round(discount_base -discount_comp,1)
     diff_4 = np.round(margin_base -margin_comp,1)
     
+    
 
     base_st_ct = df.loc[df['month_year']==base,'Customer Name'].drop_duplicates().count()
     comp_st_ct = df.loc[df['month_year']==comparison,'Customer Name'].drop_duplicates().count()
 
     diff_store = np.round(base_st_ct-comp_st_ct,1)
     
+    base_order_ct = df.loc[df['month_year']==base,'Order ID'].drop_duplicates().count()
+    comp_order_ct = df.loc[df['month_year']==comparison,'Order ID'].drop_duplicates().count()
+
+    diff_order = np.round(base_order_ct-comp_order_ct,1)
     
     
     store_base = store_df.loc[store_df['month_year']==base].sort_values('Sales',ascending = False).reset_index()[:10]
@@ -258,6 +268,31 @@ def update_cards(base, comparison):
     
     merged_df2=pd.merge(sub_dept_base, sub_dept_comp, on = 'Sub Category', how = 'left')
     merged_df2['diff'] = merged_df2['Sales_base']-merged_df2['Sales_comp']
+    
+    region_base = region_df.loc[region_df['month_year']==base].sort_values('Sales',ascending = False).reset_index()[:10]
+    region_base=region_base.rename(columns = {'Sales':'Sales_base'})
+    region_comp = region_df.loc[sub_dept_df['month_year']==comparison].sort_values('Sales',ascending = False).reset_index()
+    region_comp=region_comp.rename(columns = {'Sales':'Sales_comp'})
+    
+    merged_df3=pd.merge(region_base, region_comp, on = 'Region', how = 'left')
+    merged_df3['diff'] = merged_df3['Sales_base']-merged_df3['Sales_comp']
+    
+    city_base = city_df.loc[city_df['month_year']==base].sort_values('Sales',ascending = False).reset_index()[:10]
+    city_base=city_base.rename(columns = {'Sales':'Sales_base'})
+    city_comp = city_df.loc[city_df['month_year']==comparison].sort_values('Sales',ascending = False).reset_index()
+    city_comp=city_comp.rename(columns = {'Sales':'Sales_comp'})
+    
+    merged_df4=pd.merge(city_base, city_comp, on = 'City', how = 'left')
+    merged_df4['diff'] = merged_df4['Sales_base']-merged_df4['Sales_comp']
+    
+    order_base = order_df.loc[order_df['month_year']==base].sort_values('Sales',ascending = False).reset_index()[:10]
+    order_base=order_base.rename(columns = {'Sales':'Sales_base'})
+    order_comp = order_df.loc[order_df['month_year']==comparison].sort_values('Sales',ascending = False).reset_index()
+    order_comp=order_comp.rename(columns = {'Sales':'Sales_comp'})
+    
+    merged_df5=pd.merge(order_base, order_comp, on = 'Order ID', how = 'left')
+    merged_df5['diff'] = merged_df5['Sales_base']-merged_df5['Sales_comp']
+    
     
     fig = go.Figure(data = [go.Bar(x = sales_base1['month_year'], y = sales_base1['Sales'],\
                                    base = dict(marker_color = '#FA8072', color = '#FA8072', width = 4),name = '{}'.format(base)),
@@ -323,7 +358,29 @@ def update_cards(base, comparison):
                       xaxis_ticksuffix = ' '
                      )
 
-
+    fig6 = go.Figure([go.Bar(x = merged_df3['diff'], y = merged_df3['Region'], marker_color = '#4863A0',\
+                              orientation = 'h',
+                             textposition = 'outside'
+                             ),
+                 ])
+        
+    fig6.update_layout(plot_bgcolor = 'white',
+                       margin=dict(l = 40, r = 5, t = 60, b = 40),
+                      xaxis_tickprefix = 'INR ',
+                      xaxis_ticksuffix = ' '
+                     )
+    
+    fig7 = go.Figure([go.Bar(x = merged_df4['diff'], y = merged_df4['City'], marker_color = '#4863A0',\
+                              orientation = 'h',
+                             textposition = 'outside'
+                             ),
+                 ])
+        
+    fig7.update_layout(plot_bgcolor = 'white',
+                       margin=dict(l = 40, r = 5, t = 60, b = 40),
+                      xaxis_tickprefix = 'INR ',
+                      xaxis_ticksuffix = ' '
+                     )
     
     if diff_1 >= 0:
         a =   dcc.Markdown( dangerously_allow_html = True,
@@ -351,6 +408,15 @@ def update_cards(base, comparison):
                    children = ["<sub>-{0}{1}{2}</sub>".format('INR ',np.abs(diff_1),' ')], style = {'textAlign':'center'})
                           
         
+    if diff_4 >= 0:
+        a =   dcc.Markdown( dangerously_allow_html = True,
+                   children = ["<sub>+{0}{1}{2}</sub>".format('INR ',diff_1,' ')], style = {'textAlign':'center'})
+        
+    elif diff_4 < 0:
+        
+        a =    dcc.Markdown( dangerously_allow_html = True,
+                   children = ["<sub>-{0}{1}{2}</sub>".format('INR ',np.abs(diff_1),' ')], style = {'textAlign':'center'})
+                          
     if diff_store >= 0:
         c =   dcc.Markdown( dangerously_allow_html = True,
                    children = ["<sub>+{0}</sub>".format(diff_store)], style = {'textAlign':'center'})
@@ -424,16 +490,29 @@ def update_cards(base, comparison):
             [
                 html.H6('Total Customers', style = {'fontWeight':'bold', 'textAlign':'center'}),
                 
-                html.H3('{0}'.format( base_st_ct), style = {'color':'#090059','textAlign':'center'}),
+                html.H5('{0}'.format( base_st_ct), style = {'color':'#090059','textAlign':'center'}),
                 
                 c
-                
                 ]
                    
             )  
         ]
     
     card_content6 = [
+        
+        dbc.CardBody(
+            [
+                html.H6('Total Orders', style = {'fontWeight':'bold', 'textAlign':'center'}),
+                
+                html.H5('{0}'.format( base_order_ct), style = {'color':'#090059','textAlign':'center'}),
+                
+                c
+                ]
+                   
+            )  
+        ]
+    
+    card_content7 = [
         
         dbc.CardBody(
             [
@@ -447,7 +526,7 @@ def update_cards(base, comparison):
                    
             )  
         ]
-    card_content7 = [
+    card_content8 = [
         
         dbc.CardBody(
             [
@@ -462,7 +541,51 @@ def update_cards(base, comparison):
             )  
         ]
     
+    card_content9 = [
+        
+        dbc.CardBody(
+            [
+                html.H6('Monthly Sales Comparison', style = {'fontWeight':'bold', 'textAlign':'center'}),
+                
+                dcc.Graph(figure = fig, style = {'height':'250px'})
+                
+                
+                ]
+                   
+            )  
+        ] 
+    
+    card_content10 = [
+        
+        dbc.CardBody(
+            [
+                html.H6('Sales difference between Categories ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
+                
+                dcc.Graph(figure = fig4, style = {'height':'300px'})
+                
+                
+                ]
+                   
+            )  
+        ]
+    
+    
+    
     card_content11 = [
+        
+        dbc.CardBody(
+            [
+                html.H6('Sales difference between Sub Categories ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
+                
+                dcc.Graph(figure = fig5, style = {'height':'290px'})
+                
+                
+                ]
+                   
+            )  
+        ] 
+    
+    card_content12 = [
         
         dbc.CardBody(
             [
@@ -483,27 +606,14 @@ def update_cards(base, comparison):
             )  
         ]
     
-    card_content9 = [
-        
-        dbc.CardBody(
-            [
-                html.H6('Sales difference between Categories ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
-                
-                dcc.Graph(figure = fig4, style = {'height':'300px'})
-                
-                
-                ]
-                   
-            )  
-        ]
     
-    card_content8 = [
+    card_content13 = [
         
         dbc.CardBody(
             [
-                html.H6('Monthly Sales Comparison', style = {'fontWeight':'bold', 'textAlign':'center'}),
+                html.H6('Sales difference between Regions ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
                 
-                dcc.Graph(figure = fig, style = {'height':'250px'})
+                dcc.Graph(figure = fig6, style = {'height':'300px'})
                 
                 
                 ]
@@ -511,23 +621,22 @@ def update_cards(base, comparison):
             )  
         ] 
     
-    card_content10 = [
+    card_content14 = [
         
         dbc.CardBody(
             [
-                html.H6('Sales difference between Sub Categories ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
+                html.H6('Sales difference between Cities ({} - {})'.format(base, comparison), style = {'fontWeight':'bold', 'textAlign':'center'}),
                 
-                dcc.Graph(figure = fig5, style = {'height':'300px'})
+                dcc.Graph(figure = fig7, style = {'height':'300px'})
                 
                 
                 ]
                    
             )  
         ] 
-    
 
     
-    return card_content, card_content2,card_content3, card_content4, card_content5, card_content6, card_content7, card_content8,card_content9, card_content10, card_content11
+    return card_content, card_content2,card_content3, card_content4, card_content5, card_content6, card_content7, card_content8,card_content9, card_content10, card_content11,card_content12, card_content13, card_content14
 
 
 if __name__ == "__main__":
